d27929aa7c330abd809fed1498b19cb175d4254c
Nabille Helal <nabilleh@outlook.com>
2019-10-15 14:02:41 +0100

    Add temperature up spec

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,8 +6,17 @@ describe('Thermostat', function() {
   });
 
   describe('temperature', function() {
+
     it('defaults to 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20)
     });
   });
+
+  describe('up', function() {
+    it("should increase the temperature when selected", function() {
+      thermostat.up;
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+
 });
