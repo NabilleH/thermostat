74cbfec3260e8bd8089731e707db02c8bd710ec5
Nabille Helal <nabilleh@outlook.com>
2019-10-16 14:20:22 +0100

    Add weather API

--- src/interface.js
+++ src/interface.js
@@ -38,4 +38,31 @@ $(document).ready(function() {
     $('#usage').attr('class', thermostat.energyStatus());
   };
 
+  // function displayWeather(city) {
+  //   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  //   var token = '11669ba4faa83f11dfef243d15eace9f';
+  //   var units = '&units=metric';
+  //   $.get(url + token + units, function(data) {
+  //   $('#current-temperature').text(data.main.temp);
+  // })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp)
+    })
+  })
+
+  // displayWeather('London');
+  //
+  // $('#select-city').submit(function(event) {
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   displayWeather(city);
+  // })
+
 });
